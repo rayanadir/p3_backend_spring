ffef358c33973854437fdd241e5a5be67f99e6b4
rayanadir <rayan.dahmena@gmail.com>
2023-07-26 22:33:12 +0200

    add spring security configuration

---
+++ src/main/java/com/rayandahmena/project_3/configuration/SpringSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.rayandahmena.project_3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/owner").hasRole("OWNER")
+                .requestMatchers("/tenant").hasRole("TENANT")
+                .anyRequest()
+                .authenticated());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+            .withUser("owneruser").password(passwordEncoder().encode("owner123")).roles("OWNER")
+            .and()
+            .withUser("tenantuser").password(passwordEncoder().encode("tenant123")).roles("TENANT");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
