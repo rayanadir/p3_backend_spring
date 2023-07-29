14489a2beb2521eae119cd67c3ff71a9d492d13f
rayanadir <rayan.dahmena@gmail.com>
2023-07-29 18:45:26 +0200

    import bcrypt

--- src/main/java/com/rayandahmena/project_3/configuration/SpringSecurityConfig.java
+++ src/main/java/com/rayandahmena/project_3/configuration/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.rayandahmena.project_3.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
