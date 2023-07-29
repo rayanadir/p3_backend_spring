14366af0bc0b502319ac6d95d3e254b693ddfdff
rayanadir <rayan.dahmena@gmail.com>
2023-07-29 19:31:19 +0200

    user entity class

---
+++ src/main/java/com/rayandahmena/project_3/model/User.java
@@ -0,0 +1,53 @@
+package com.rayandahmena.project_3.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "name", nullable = false,length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
