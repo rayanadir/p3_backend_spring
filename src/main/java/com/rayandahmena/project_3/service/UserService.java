76820573256b16b7071fc80737daaeff0b8ebeb0
rayanadir <rayan.dahmena@gmail.com>
2023-08-04 19:28:45 +0200

    add user register method by dto

--- src/main/java/com/rayandahmena/project_3/service/UserService.java
+++ src/main/java/com/rayandahmena/project_3/service/UserService.java
@@ -1,36 +1,41 @@
 package com.rayandahmena.project_3.service;
 
+import com.rayandahmena.project_3.dto.RegisterDTO;
 import com.rayandahmena.project_3.entity.User;
 import com.rayandahmena.project_3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
-public class UserDetail implements UserDetailsService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
 
         User user = userRepository.findByEmail(username);
 
-        if(user == null){
+        if(user == null)
             throw new UsernameNotFoundException("User doesn't exist by this name");
-        }
 
-        Set<GrantedAuthority> authorities = user.getRoles().stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+        return user;
+    }
 
-        return new org.springframework.security.core.userdetails.User(username,user.getPassword(),authorities);
+    public User register(RegisterDTO dto){
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        String password = passwordEncoder.encode(dto.getPassword());
+        user.setPassword(password);
+        return userRepository.save(user);
     }
 }
