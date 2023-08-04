d89cf5380fa7555c074466294d2821a284529989
rayanadir <rayan.dahmena@gmail.com>
2023-08-04 22:58:59 +0200

    add me controller

---
+++ src/main/java/com/rayandahmena/project_3/controller/MeController.java
@@ -0,0 +1,20 @@
+package com.rayandahmena.project_3.controller;
+
+import com.rayandahmena.project_3.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class MeController {
+
+    private UserService userService;
+
+    @RequestMapping(value="/auth/me", method = RequestMethod.GET)
+    public UserDetails me(Authentication authentication){
+        return userService.loadUserByUsername(authentication.getName());
+    }
+}
