d89c145f89d718d79e568ec0b07047ac947120d3
rayanadir <rayan.dahmena@gmail.com>
2023-08-05 22:49:32 +0200

    add UserDTO

---
+++ src/main/java/com/rayandahmena/project_3/dto/UserDTO.java
@@ -0,0 +1,65 @@
+package com.rayandahmena.project_3.dto;
+
+import java.sql.Timestamp;
+
+public class UserDTO {
+    private int id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
