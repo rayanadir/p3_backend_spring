301e2782be325e162794b9386fa715af56670d0b
rayanadir <rayan.dahmena@gmail.com>
2023-08-09 00:34:01 +0200

    add RegisterController register route

--- src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
+++ src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
@@ -1,16 +1,22 @@
 package com.rayandahmena.project_3.controller;
 
+import com.rayandahmena.project_3.dto.RegisterDTO;
+import com.rayandahmena.project_3.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "auth")
+@RequestMapping(path = "api/auth")
 public class RegisterController {
-    
+    @Autowired
+    RegisterService registerService;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public void register(){
-        
+    public ResponseEntity<String> register(@RequestBody RegisterDTO dto){
+        return ResponseEntity.ok(registerService.register(dto));
     }
 }
