765a4c9e3bd4e330cb72743f0e438ffde741bece
rayanadir <rayan.dahmena@gmail.com>
2023-08-08 23:04:11 +0200

    remove unused line

--- src/main/java/com/rayandahmena/project_3/interfaces/JwtGeneratorInterface.java
+++ src/main/java/com/rayandahmena/project_3/interfaces/JwtGeneratorInterface.java
@@ -2,8 +2,6 @@ package com.rayandahmena.project_3.interfaces;
 
 import com.rayandahmena.project_3.entity.User;
 
-import java.util.Map;
-
 public interface JwtGeneratorInterface {
     String generateToken(User user);
 }
