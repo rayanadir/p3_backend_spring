353b9ecad5e831ca697f0b34c614a9200cb46d8a
rayanadir <rayan.dahmena@gmail.com>
2023-08-09 19:13:53 +0200

    remove method parameter

--- src/main/java/com/rayandahmena/project_3/controller/MeController.java
+++ src/main/java/com/rayandahmena/project_3/controller/MeController.java
@@ -2,6 +2,7 @@ package com.rayandahmena.project_3.controller;
 
 import com.rayandahmena.project_3.service.UserService;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,8 @@ public class MeController {
     private UserService userService;
 
     @RequestMapping(value="/auth/me", method = RequestMethod.GET)
-    public UserDetails me(Authentication authentication){
+    public UserDetails me(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return userService.loadUserByUsername(authentication.getName());
     }
 }
