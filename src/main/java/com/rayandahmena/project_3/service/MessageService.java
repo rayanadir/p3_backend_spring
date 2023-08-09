8a862464268ac79fbc00dd13a1507e04a416c98a
rayanadir <rayan.dahmena@gmail.com>
2023-08-09 19:14:50 +0200

    add annotations

--- src/main/java/com/rayandahmena/project_3/service/MessageService.java
+++ src/main/java/com/rayandahmena/project_3/service/MessageService.java
@@ -1,13 +1,15 @@
 package com.rayandahmena.project_3.service;
 
-import com.rayandahmena.project_3.dto.MessageDTO;
 import com.rayandahmena.project_3.entity.Message;
 import com.rayandahmena.project_3.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 
+@Service
 public class MessageService {
 
+    @Autowired
     private MessageRepository messageRepository;
 
     public Message saveMessage(Message message){
