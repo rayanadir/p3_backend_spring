8a3a726a2f9bfe3cc03404f9a93713d0d4e6dd94
rayanadir <rayan.dahmena@gmail.com>
2023-08-09 19:08:19 +0200

    add constants

---
+++ src/main/java/com/rayandahmena/project_3/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.rayandahmena.project_3.constants;
+
+public class Constants {
+    public static final String SECRET = "SECRET_KEY";
+    public static final int EXPIRATION_TIME = 1000*60*15; // 15 minutes
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
