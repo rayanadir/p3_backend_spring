1f087831bceae6ae9f72b97c02f11034875f0d8b
rayanadir <rayan.dahmena@gmail.com>
2023-08-09 19:12:05 +0200

    add getters + setters

--- src/main/java/com/rayandahmena/project_3/entity/Rental.java
+++ src/main/java/com/rayandahmena/project_3/entity/Rental.java
@@ -88,4 +88,12 @@ public class Rental {
     public void setOwner_id(int owner_id) {
         this.owner_id = owner_id;
     }
+
+    public Timestamp getCreated_at() { return created_at; }
+
+    public void setCreated_at(Timestamp created_at) { this.created_at = created_at; }
+
+    public Timestamp getUpdated_at() { return updated_at; }
+
+    public void setUpdated_at(Timestamp updated_at) { this.updated_at = updated_at; }
 }
