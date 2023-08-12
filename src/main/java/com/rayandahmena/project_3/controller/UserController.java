11d661b5619aa1dc22d6212bb8c6699408425461
rayanadir <rayan.dahmena@gmail.com>
2023-08-13 00:59:39 +0200

    remove unused import

--- src/main/java/com/rayandahmena/project_3/controller/UserController.java
+++ src/main/java/com/rayandahmena/project_3/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.rayandahmena.project_3.controller;
 
 import com.rayandahmena.project_3.entity.User;
+import com.rayandahmena.project_3.entity.response.UserResponse;
 import com.rayandahmena.project_3.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api")
@@ -16,7 +16,8 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public Optional<User> getUser(@PathVariable int id){
-        return userService.getUser(id);
+    public ResponseEntity<UserResponse> getUser(@PathVariable int id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(new UserResponse(user));
     }
 }
