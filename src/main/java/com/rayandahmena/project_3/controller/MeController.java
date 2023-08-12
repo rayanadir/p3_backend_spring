bd488504e3934a0a1dc830258ad41f918d4e75e8
rayanadir <rayan.dahmena@gmail.com>
2023-08-13 01:03:19 +0200

    change return type

--- src/main/java/com/rayandahmena/project_3/controller/MeController.java
+++ src/main/java/com/rayandahmena/project_3/controller/MeController.java
@@ -1,9 +1,10 @@
 package com.rayandahmena.project_3.controller;
 
+import com.rayandahmena.project_3.entity.User;
+import com.rayandahmena.project_3.entity.response.UserResponse;
 import com.rayandahmena.project_3.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,9 @@ public class MeController {
     private UserService userService;
 
     @RequestMapping(value="/auth/me", method = RequestMethod.GET)
-    public UserDetails me(){
+    public UserResponse me(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return userService.loadUserByUsername(authentication.getName());
+        User user = userService.findByEmail(authentication.getName());
+        return new UserResponse(user);
     }
 }
