56e39e371a4a5b711984415f343dbae4b8359936
rayanadir <rayan.dahmena@gmail.com>
2023-08-13 00:31:57 +0200

    change method

--- src/main/java/com/rayandahmena/project_3/service/MessageService.java
+++ src/main/java/com/rayandahmena/project_3/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.rayandahmena.project_3.service;
 
+import com.rayandahmena.project_3.dto.MessageDTO;
 import com.rayandahmena.project_3.entity.Message;
 import com.rayandahmena.project_3.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,9 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public Message saveMessage(Message message){
-       return messageRepository.save(message);
+    public Message newMessage(MessageDTO messageDTO){
+        Message message = new Message(messageDTO);
+        messageRepository.save(message);
+        return message;
     }
 }
