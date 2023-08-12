fc9b7c618b3dc474f47e45df2642a83b44be1bed
rayanadir <rayan.dahmena@gmail.com>
2023-08-12 22:11:16 +0200

    add RentalService

---
+++ src/main/java/com/rayandahmena/project_3/service/RentalService.java
@@ -0,0 +1,17 @@
+package com.rayandahmena.project_3.service;
+
+import com.rayandahmena.project_3.repository.RentalsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RentalService {
+
+    @Autowired
+    private RentalsRepository rentalsRepository;
+
+
+
+
+}
