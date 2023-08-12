6c9b8aa22b8513b4caa724c683d63cfc09134227
rayanadir <rayan.dahmena@gmail.com>
2023-08-13 00:30:51 +0200

    add NewMessageRequest

---
+++ src/main/java/com/rayandahmena/project_3/entity/request/NewMessageRequest.java
@@ -0,0 +1,34 @@
+package com.rayandahmena.project_3.entity.request;
+
+public class NewMessageRequest {
+
+    private String message;
+
+    private int user_id;
+
+    private int rental_id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRental_id() {
+        return rental_id;
+    }
+
+    public void setRental_id(int rental_id) {
+        this.rental_id = rental_id;
+    }
+}
