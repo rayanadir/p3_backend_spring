94f863cda05e139f0e91dd77f9622945f9bd5d9c
rayanadir <rayan.dahmena@gmail.com>
2023-08-12 23:43:46 +0200

    add getRental + getAllRentals methods

--- src/main/java/com/rayandahmena/project_3/service/RentalService.java
+++ src/main/java/com/rayandahmena/project_3/service/RentalService.java
@@ -1,8 +1,11 @@
 package com.rayandahmena.project_3.service;
 
+import com.rayandahmena.project_3.entity.Rental;
 import com.rayandahmena.project_3.repository.RentalsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -11,7 +14,15 @@ public class RentalService {
     @Autowired
     private RentalsRepository rentalsRepository;
 
+    public Rental getRental(int id){
+        return rentalsRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Error ! Rental not found : " + id)
+        );
+    }
 
+    public List<Rental> getAllRentals(){
+        return rentalsRepository.findAll();
+    }
 
 
 }
