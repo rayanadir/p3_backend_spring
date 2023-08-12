c7d1805076d50d994ca33379807044e7cbe596c5
rayanadir <rayan.dahmena@gmail.com>
2023-08-12 21:49:51 +0200

    change configure method

--- src/main/java/com/rayandahmena/project_3/configuration/SpringSecurityConfig.java
+++ src/main/java/com/rayandahmena/project_3/configuration/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.rayandahmena.project_3.configuration;
 
 import com.rayandahmena.project_3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,17 +21,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/api/auth/login").permitAll()
-                .requestMatchers("/api/auth/register").permitAll()
-                .requestMatchers("/api/**").authenticated()
-                .anyRequest()
-                .authenticated()).formLogin((form) ->form.loginPage("/login").permitAll());
+        http.cors().and().csrf().disable().authorizeHttpRequests()
+                .antMatchers("/api/auth/login").permitAll()
+                .antMatchers("/api/auth/register").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new UserService();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
