29b028a7200c3ab24f87012dbc2c1e4762e61fbe
rayanadir <rayan.dahmena@gmail.com>
2023-08-14 00:15:09 +0200

    add swagger config

---
+++ src/main/java/com/rayandahmena/project_3/configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.rayandahmena.project_3.configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(new Info()
+                .title("ChaTôp API Documentation")
+                .description("API Documentation for ChaTôp application (Swagger)")
+                .version("1.0")
+        );
+    }
+}
