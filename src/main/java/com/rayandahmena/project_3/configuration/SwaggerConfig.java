6a3f4578acc9356f989a04adac2ccfa36d35ce44
rayanadir <rayan.dahmena@gmail.com>
2023-08-14 19:17:39 +0200

    add @OpenAPIDefinition

--- src/main/java/com/rayandahmena/project_3/configuration/SwaggerConfig.java
+++ src/main/java/com/rayandahmena/project_3/configuration/SwaggerConfig.java
@@ -1,11 +1,12 @@
 package com.rayandahmena.project_3.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfig {
 
