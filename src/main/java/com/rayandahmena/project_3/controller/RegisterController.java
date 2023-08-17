1c1005871dc88609cad4524775c11fe8bb378e8d
rayanadir <rayan.dahmena@gmail.com>
2023-08-17 21:28:59 +0200

    change method

--- src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
+++ src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
@@ -1,14 +1,10 @@
 package com.rayandahmena.project_3.controller;
 
 import com.rayandahmena.project_3.dto.RegisterDTO;
-import com.rayandahmena.project_3.dto.TokenDTO;
 import com.rayandahmena.project_3.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/auth")
@@ -16,10 +12,8 @@ public class RegisterController {
     @Autowired
     RegisterService registerService;
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<TokenDTO> register(@RequestBody RegisterDTO dto){
-        TokenDTO token = new TokenDTO();
-        token.setToken(registerService.register(dto));
-        return ResponseEntity.ok(token);
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public ResponseEntity<String> register(@RequestBody RegisterDTO dto){
+        return ResponseEntity.ok(registerService.register(dto));
     }
 }
