71f8832df6d7ab09f68a59f27bb75ea9b49564bb
rayanadir <rayan.dahmena@gmail.com>
2023-08-18 00:40:31 +0200

    check if email is available

--- src/main/java/com/rayandahmena/project_3/service/RegisterService.java
+++ src/main/java/com/rayandahmena/project_3/service/RegisterService.java
@@ -1,7 +1,9 @@
 package com.rayandahmena.project_3.service;
 
 import com.rayandahmena.project_3.dto.RegisterDTO;
+import com.rayandahmena.project_3.dto.TokenDTO;
 import com.rayandahmena.project_3.entity.User;
+import com.rayandahmena.project_3.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,19 @@ public class RegisterService {
     @Autowired
     JwtGeneratorService jwtGeneratorService;
 
+    @Autowired
+    UserRepository userRepository;
+
     public String register(RegisterDTO dto){
         User user = new User();
         user.setName(dto.getName());
         user.setEmail(dto.getEmail());
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        userService.createNewUser(user);
-
-        return jwtGeneratorService.generateToken(user);
+        if(userService.isEmailAvailable(user)){
+            userService.createNewUser(user);
+            return jwtGeneratorService.generateToken(user);
+        }else{
+            return "Email already exists !";
+        }
     }
 }
