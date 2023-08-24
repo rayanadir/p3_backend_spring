8643de07f6619f5cbb4e7f9b03b5ad6177f7909d
rayanadir <rayan.dahmena@gmail.com>
2023-08-24 14:12:20 +0200

    add getters + setters + constructor

--- src/main/java/com/rayandahmena/project_3/entity/response/UserResponse.java
+++ src/main/java/com/rayandahmena/project_3/entity/response/UserResponse.java
@@ -3,6 +3,7 @@ package com.rayandahmena.project_3.entity.response;
 import com.rayandahmena.project_3.entity.User;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class UserResponse {
     private int id;
@@ -13,7 +14,49 @@ public class UserResponse {
 
     private Timestamp created_at;
 
-    private  Timestamp updated_at;
+    private Timestamp updated_at;
+
+    public UserResponse(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreated_at() { return new SimpleDateFormat("yyyy/MM/dd").format(created_at) ; }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return new SimpleDateFormat("yyyy/MM/dd").format(updated_at);
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 
     public UserResponse(User user){
         super();
