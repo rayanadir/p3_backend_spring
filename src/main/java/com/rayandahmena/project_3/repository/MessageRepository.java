6f6f27db2ba28c7534b4971d1b0907574bdada44
rayanadir <rayan.dahmena@gmail.com>
2023-08-26 16:59:33 +0200

    add @Repository

--- src/main/java/com/rayandahmena/project_3/repository/MessageRepository.java
+++ src/main/java/com/rayandahmena/project_3/repository/MessageRepository.java
@@ -2,5 +2,7 @@ package com.rayandahmena.project_3.repository;
 
 import com.rayandahmena.project_3.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message,Integer> {}
--- src/main/java/com/rayandahmena/project_3/repository/RentalsRepository.java
+++ src/main/java/com/rayandahmena/project_3/repository/RentalsRepository.java
@@ -2,6 +2,8 @@ package com.rayandahmena.project_3.repository;
 
 import com.rayandahmena.project_3.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RentalsRepository extends JpaRepository<Rental,Integer> {
 }
