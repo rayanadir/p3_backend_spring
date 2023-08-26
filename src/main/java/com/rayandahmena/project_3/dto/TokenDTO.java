9c4085fa577e6da799ead478e877521f451d9179
rayanadir <rayan.dahmena@gmail.com>
2023-08-26 16:59:08 +0200

    add constructor

--- src/main/java/com/rayandahmena/project_3/dto/TokenDTO.java
+++ src/main/java/com/rayandahmena/project_3/dto/TokenDTO.java
@@ -3,6 +3,11 @@ package com.rayandahmena.project_3.dto;
 public class TokenDTO {
     private String token;
 
+    public TokenDTO(String token){
+        super();
+        this.token = token;
+    }
+
     public void setToken(String token) {
         this.token = token;
     }
