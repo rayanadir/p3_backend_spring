889decb37bf17de33a1113001c780cbdd1b69051
rayanadir <rayan.dahmena@gmail.com>
2023-08-26 17:01:08 +0200

    change controller methods

--- src/main/java/com/rayandahmena/project_3/controller/RentalController.java
+++ src/main/java/com/rayandahmena/project_3/controller/RentalController.java
@@ -3,6 +3,7 @@ package com.rayandahmena.project_3.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rayandahmena.project_3.dto.RentalDTO;
 import com.rayandahmena.project_3.entity.Rental;
+import com.rayandahmena.project_3.entity.User;
 import com.rayandahmena.project_3.entity.request.NewRentalRequest;
 import com.rayandahmena.project_3.service.RentalService;
 import com.rayandahmena.project_3.service.UserService;
@@ -10,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -43,25 +46,15 @@ public class RentalController {
     }
 
     @RequestMapping(value = "/rentals", method = RequestMethod.POST)
-    public ResponseEntity<HashMap<String,String>> createRental(@RequestBody NewRentalRequest newRentalReq, HttpServletRequest req, Authentication auth) throws IOException {
-        int ownerId = userService.getUserId(auth);
+    public ResponseEntity<HashMap<String,String>> createRental(NewRentalRequest newRentalReq) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(authentication.getName());
+        int ownerId = user.getId();
         return ResponseEntity.ok(rentalService.createRental(newRentalReq, ownerId));
     }
 
     @RequestMapping(value = "/rentals/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<HashMap<String,String>> updateRental(@PathVariable int id, NewRentalRequest newRentalReq, Authentication auth){
-        Rental rental = rentalService.getRental(id);
-        int ownerId = rental.getOwner_id();
-        int userId = userService.findByEmail(auth.getName()).getId();
-        // if userId and ownerId are different, forbid request
-        if(!Objects.equals(ownerId,userId)){
-            return new ResponseEntity<>(new HashMap<>(), HttpStatus.UNAUTHORIZED);
-        }
-        newRentalReq.setPicture(rental.getPicture());
-        ObjectMapper objectMapper = new ObjectMapper();
-        Rental updatedRental = objectMapper.convertValue(newRentalReq, Rental.class);
-        updatedRental.setId(rental.getId());
-        updatedRental.setOwner_id(rental.getOwner_id());
-        return ResponseEntity.ok(rentalService.updateRental(updatedRental));
+    public ResponseEntity<HashMap<String,String>> updateRental(@PathVariable int id, NewRentalRequest newRentalReq){
+        return ResponseEntity.ok(rentalService.updateRental(id,newRentalReq));
     }
 }
