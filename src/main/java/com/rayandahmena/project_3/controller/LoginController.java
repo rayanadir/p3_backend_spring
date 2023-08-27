d782e700d1ae44c3dbd282a460c0564bb4dbcde2
rayanadir <rayan.dahmena@gmail.com>
2023-08-28 00:10:54 +0200

    change method return type + swagger documentation

--- src/main/java/com/rayandahmena/project_3/controller/LoginController.java
+++ src/main/java/com/rayandahmena/project_3/controller/LoginController.java
@@ -1,12 +1,16 @@
 package com.rayandahmena.project_3.controller;
 
 import com.rayandahmena.project_3.dto.LoginDTO;
+import com.rayandahmena.project_3.entity.response.TokenResponse;
 import com.rayandahmena.project_3.service.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 
 @RestController
 @RequestMapping(path="/api/auth")
@@ -15,8 +19,23 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
+    @Operation(summary = "Login user by email & password")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Successful login",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TokenResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "401", description = "Login failed", content = @Content
+            )
+    })
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public ResponseEntity<HashMap<String,String>> login(@RequestBody LoginDTO dto){
-        return ResponseEntity.ok(loginService.login(dto));
+    public TokenResponse login(@RequestBody LoginDTO dto){
+        return new TokenResponse(loginService.login(dto));
     }
 }
