45c39708fc1f708bb25f490096a3c92fe13e3db8
rayanadir <rayan.dahmena@gmail.com>
2023-08-27 15:17:22 +0200

    change return type

--- src/main/java/com/rayandahmena/project_3/controller/MessageController.java
+++ src/main/java/com/rayandahmena/project_3/controller/MessageController.java
@@ -4,12 +4,20 @@ import com.rayandahmena.project_3.dto.MessageDTO;
 import com.rayandahmena.project_3.entity.Rental;
 import com.rayandahmena.project_3.entity.User;
 import com.rayandahmena.project_3.entity.request.NewMessageRequest;
+import com.rayandahmena.project_3.entity.response.MessageResponse;
 import com.rayandahmena.project_3.service.MessageService;
 import com.rayandahmena.project_3.service.RentalService;
 import com.rayandahmena.project_3.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,24 +30,33 @@ import java.util.Objects;
 @RequestMapping("api")
 public class MessageController {
 
+    @Autowired
     private MessageService messageService;
 
-    private UserService userService;
-
-    private RentalService rentalService;
-
+    @Operation(summary = "Send message")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Message successfully sent",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "Message not sent", content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "401", description = "Message not sent", content = @Content
+            )
+    })
     @RequestMapping(value="/messages", method = RequestMethod.POST)
-    public ResponseEntity<HashMap<String,String>> sendMessage(@RequestBody NewMessageRequest newMessageReq, Authentication auth){
-        User user = userService.findById(newMessageReq.getUser_id());
-        // If different user id, forbid authorization
-        if(!Objects.equals(userService.findByEmail(auth.getName()).getId(), user.getId())){
-            return new ResponseEntity<>(new HashMap<>(), HttpStatus.UNAUTHORIZED);
-        }
-        Rental rental = rentalService.getRental(newMessageReq.getRental_id());
-        MessageDTO messageDTO = new MessageDTO(newMessageReq.getMessage(), user.getId(), rental.getId());
-        messageService.newMessage(messageDTO);
-        HashMap<String,String> response = new HashMap<String,String>();
-        response.put("message", "Message sent with success");
-        return ResponseEntity.ok(response);
+    public HashMap<String, String> sendMessage(@RequestBody NewMessageRequest newMessageReq){
+        /*String message = newMessageReq.getMessage();
+        int rental_id = rental.getId();
+        int user_id= user.getId();
+        MessageDTO messageDTO = new MessageDTO(rental_id, user_id, message);*/
+        return messageService.newMessage(newMessageReq);
     }
 }
--- src/main/java/com/rayandahmena/project_3/service/MessageService.java
+++ src/main/java/com/rayandahmena/project_3/service/MessageService.java
@@ -2,9 +2,19 @@ package com.rayandahmena.project_3.service;
 
 import com.rayandahmena.project_3.dto.MessageDTO;
 import com.rayandahmena.project_3.entity.Message;
+import com.rayandahmena.project_3.entity.Rental;
+import com.rayandahmena.project_3.entity.User;
+import com.rayandahmena.project_3.entity.request.NewMessageRequest;
+import com.rayandahmena.project_3.entity.response.MessageResponse;
 import com.rayandahmena.project_3.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Objects;
 
 
 @Service
@@ -13,9 +23,17 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public Message newMessage(MessageDTO messageDTO){
-        Message message = new Message(messageDTO);
+    public HashMap<String, String> newMessage(NewMessageRequest messageReq){
+        Message message = new Message();
+        message.setMessage(messageReq.getMessage());
+        message.setUser_id(messageReq.getUser_id());
+        message.setRental_id(messageReq.getRental_id());
+        message.setCreated_at(new Timestamp(System.currentTimeMillis()));
+        message.setUpdated_at(new Timestamp(System.currentTimeMillis()));
         messageRepository.save(message);
-        return message;
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "Message sent with success");
+        MessageResponse res = new MessageResponse(response);
+        return response;
     }
 }
