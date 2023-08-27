444ba65eb19efc3c6aa0bf27b1637825cb424db9
rayanadir <rayan.dahmena@gmail.com>
2023-08-27 23:55:56 +0200

    change method return type + swagger documentation

--- src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
+++ src/main/java/com/rayandahmena/project_3/controller/RegisterController.java
@@ -1,12 +1,19 @@
 package com.rayandahmena.project_3.controller;
 
 import com.rayandahmena.project_3.dto.RegisterDTO;
+
+import com.rayandahmena.project_3.entity.response.TokenResponse;
 import com.rayandahmena.project_3.service.RegisterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+
 
 @RestController
 @RequestMapping(path = "api/auth")
@@ -14,8 +21,23 @@ public class RegisterController {
     @Autowired
     RegisterService registerService;
 
+    @Operation(summary = "Register user by name, email & password")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Successful register",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TokenResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "Register failed", content = @Content
+            )
+    })
     @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public ResponseEntity<HashMap<String,String>> register(@RequestBody RegisterDTO dto){
-        return ResponseEntity.ok(registerService.register(dto));
+    public TokenResponse register(@RequestBody RegisterDTO dto){
+        return new TokenResponse(registerService.register(dto));
     }
 }
