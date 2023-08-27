3226f3566e4adb1567d3199b177d1ee8991dc9f3
rayanadir <rayan.dahmena@gmail.com>
2023-08-27 23:50:39 +0200

    add toke response

---
+++ src/main/java/com/rayandahmena/project_3/entity/response/TokenResponse.java
@@ -0,0 +1,17 @@
+package com.rayandahmena.project_3.entity.response;
+
+public class TokenResponse {
+    private String token;
+
+    public TokenResponse(String token){
+        this.token=token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
