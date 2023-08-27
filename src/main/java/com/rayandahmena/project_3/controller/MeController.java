5d81e0b05f68624a2ba973f52d8d45cb4f6b8908
rayanadir <rayan.dahmena@gmail.com>
2023-08-28 00:09:29 +0200

    add swagger documentation

--- src/main/java/com/rayandahmena/project_3/controller/MeController.java
+++ src/main/java/com/rayandahmena/project_3/controller/MeController.java
@@ -3,7 +3,11 @@ package com.rayandahmena.project_3.controller;
 import com.rayandahmena.project_3.entity.User;
 import com.rayandahmena.project_3.entity.response.UserResponse;
 import com.rayandahmena.project_3.service.UserService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,6 +22,21 @@ public class MeController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Get user info")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Get user info with success",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "401", description = "Error", content = @Content
+            )
+    })
     @RequestMapping(value="/auth/me", method = RequestMethod.GET)
     public UserResponse me(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
