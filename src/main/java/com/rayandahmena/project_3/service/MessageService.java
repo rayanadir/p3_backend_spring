7ee85a6e8e24007f0b1b134c7540248bafd337e6
rayanadir <rayan.dahmena@gmail.com>
2023-08-27 23:57:08 +0200

    change method return type

--- src/main/java/com/rayandahmena/project_3/service/MessageService.java
+++ src/main/java/com/rayandahmena/project_3/service/MessageService.java
@@ -1,20 +1,17 @@
 package com.rayandahmena.project_3.service;
 
-import com.rayandahmena.project_3.dto.MessageDTO;
+
 import com.rayandahmena.project_3.entity.Message;
-import com.rayandahmena.project_3.entity.Rental;
-import com.rayandahmena.project_3.entity.User;
+
 import com.rayandahmena.project_3.entity.request.NewMessageRequest;
-import com.rayandahmena.project_3.entity.response.MessageResponse;
+
 import com.rayandahmena.project_3.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.Objects;
 
 
 @Service
@@ -23,17 +20,13 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public HashMap<String, String> newMessage(NewMessageRequest messageReq){
+    public Message newMessage(NewMessageRequest messageReq){
         Message message = new Message();
         message.setMessage(messageReq.getMessage());
         message.setUser_id(messageReq.getUser_id());
         message.setRental_id(messageReq.getRental_id());
         message.setCreated_at(new Timestamp(System.currentTimeMillis()));
         message.setUpdated_at(new Timestamp(System.currentTimeMillis()));
-        messageRepository.save(message);
-        HashMap<String, String> response = new HashMap<>();
-        response.put("message", "Message sent with success");
-        MessageResponse res = new MessageResponse(response);
-        return response;
+        return messageRepository.save(message);
     }
 }
