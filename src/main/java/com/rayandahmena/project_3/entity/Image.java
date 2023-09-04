86071d8b017d17d82d70086d0844bdf2596522ee
rayanadir <rayan.dahmena@gmail.com>
2023-09-04 15:47:26 +0200

    add Image entity

---
+++ src/main/java/com/rayandahmena/project_3/entity/Image.java
@@ -0,0 +1,32 @@
+package com.rayandahmena.project_3.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="LOCATION")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
