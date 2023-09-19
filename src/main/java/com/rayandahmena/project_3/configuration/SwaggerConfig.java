34db0a5f590cac28b4d646c47a95deff69df7bc9
rayanadir <rayan.dahmena@gmail.com>
2023-09-19 13:47:15 +0200

    add JWT verification

--- src/main/java/com/rayandahmena/project_3/configuration/SwaggerConfig.java
+++ src/main/java/com/rayandahmena/project_3/configuration/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.rayandahmena.project_3.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,10 +22,23 @@ public class SwaggerConfig {
      */
     @Bean
     public OpenAPI openAPI(){
-        return new OpenAPI().info(new Info()
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
                 .title("ChaTôp API Documentation")
                 .description("API Documentation for ChaTôp application (Swagger)")
                 .version("1.0")
         );
+    }
+
+    /**
+     * Configure JWT SecurityScheme
+     * @return SecurityScheme
+     */
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
